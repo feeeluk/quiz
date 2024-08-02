3c52d9def393c2e2f5063fa138497331056ef243
Philip Henning <philiphenning@outlook.com>
2024-08-02 12:33:11 +0100

    styling

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,7 @@
 export function Footer(){
     return(
         <div className="Footer">
-            <p>This is V2 of WhoWantsToBeLessOfAPeasant? V1 was created as a showcase project - see 'About' for more.</p>
+            <p>This is V2 of WhoWantsToBeLessOfAPeasant? V1 was created as a showcase project - see About for more.</p>
         </div>
         
     )
