f9db4173860bafc552e04c3e52790276a4aa5c5d
Philip Henning <philiphenning@outlook.com>
2024-08-02 22:13:08 +0100

    logic used failed to deploy successfully, working on a fix - attempt 1

--- src/app/components/Question.jsx
+++ src/app/components/Question.jsx
@@ -5,42 +5,34 @@ import Image from "next/image"
 import { AddUser } from "./AddUser"
 import { UpdateLeaderboard } from "./UpdateLeaderboard"
 import { useUser } from "@clerk/nextjs"
+import { useState, useEffect } from "react"
 
-export function Question({quizID, name, category, question_number, value, question, answer_1, answer_2, answer_3, answer_4, final_answer, score, image}){
+export function Question({quizID, question_number, value, question, answer_1, answer_2, answer_3, answer_4, final_answer, score, image}){
 
     const router = useRouter()
-    const { user } = useUser();  
-    
-    async function handleAnswer(a_userID, a_username, a_quizID, a_question_number, a_score, a_answer, a_final_answer){
-        
-        // win
-        if(a_question_number === "15" && a_answer === a_final_answer){
-            a_score = parseInt(a_score, 10) + value
-            await AddUser(a_userID, a_username)
-            UpdateLeaderboard(a_userID, a_quizID, 1, a_score, a_question_number )
-            router.push("/pages/static/won")
-        }
-        
-        // answer correctly
-        else if(a_answer === a_final_answer){
-            a_score = parseInt(a_score, 10) + value
-            router.push(`/pages/dynamic/quiz/${a_quizID}/${parseInt(a_question_number) +1}?score=${a_score}`)
-        }
-        
-        // lose
-        else{    
-            await AddUser(a_userID, a_username)
-            UpdateLeaderboard(a_userID, a_quizID, 3, a_score, (parseInt(a_question_number) -1) )
-            router.push("/pages/static/lost")
-        }
-    }
+    const { user } = useUser();
 
-    // quit
-    async function handleQuit(q_userID, q_username, q_quizID, q_question_number, q_score){
-        await AddUser(q_userID, q_username)
-        UpdateLeaderboard(q_userID, q_quizID, 2, q_score, (parseInt(q_question_number) -1) )
-        router.push("/pages/static/quit")
-    }
+    let [quit, setQuit] = useState(0)  
+    
+    useEffect(() => {
+
+        const userQuit = async () => {
+
+            await AddUser(user.id, user.username)
+            UpdateLeaderboard(user.id, quizID, 2, score, (parseInt(question_number) -1) )
+            router.push("/pages/static/quit")
+        }
+
+        if(quit === 1){
+           userQuit() 
+        }
+
+    },[quit])
+
+        // Quit
+        function handleQuit(){
+            setQuit(1)
+        }
 
     return(
         <>  
@@ -56,28 +48,28 @@ export function Question({quizID, name, category, question_number, value, questi
             <div className="AnswerBox">
                 <div className="AnswerTopRow">
                     <div className="QuestionLayout Answer">
-                        <button onClick={() => handleAnswer(user.id, user.username, quizID, question_number, score, answer_1, final_answer)}>{answer_1}</button>
+                        <button onClick={() => handleAnswer()}>{answer_1}</button>
                     </div>
 
                     <div className="QuestionLayout Answer">
-                        <button onClick={() => handleAnswer(user.id, user.username, quizID, question_number, score, answer_2, final_answer)}>{answer_2}</button>
+                        <button onClick={() => handleAnswer()}>{answer_2}</button>
                     </div>
                 </div>
 
                 <div className="AnswerBottomRow">
                     <div className="QuestionLayout Answer">
-                        <button onClick={() => handleAnswer(user.id, user.username, quizID, question_number, score, answer_3, final_answer)}>{answer_3}</button>
+                        <button onClick={() => handleAnswer()}>{answer_3}</button>
                     </div>
 
                     <div className="QuestionLayout Answer">
-                        <button onClick={() => handleAnswer(user.id, user.username, quizID, question_number, score, answer_4, final_answer)}>{answer_4}</button>
+                        <button onClick={() => handleAnswer()}>{answer_4}</button>
                     </div>
                 </div>
             </div>
 
-            <h1>Score: {score}</h1>
+            <button onClick={() => handleQuit()}>QUIT</button>
 
-            <button onClick={() => handleQuit(user.id, user.username, quizID, question_number, score)}>QUIT</button>
+            <h1>Score: {score}</h1>
         </>     
     )
 }
--- src/app/pages/static/about/page.js
+++ src/app/pages/static/about/page.js
@@ -1,5 +1,14 @@
 export default function About(){
     return(
-        <h1>About</h1>
+        <>
+            <h1>About</h1>
+
+            <div>
+                <p>The orignial version of Who wants to be a peasant? was created in React as an end of term showcase project by Hannah Carter, William Stanforth-Sharpe and myself.</p>
+
+                <p>This (version 2) is my own project that I am building using Next.js, and gives me the opportunity to improve and expand upon what was a very short project.</p>
+            </div>
+        </>
+        
     )
 }
