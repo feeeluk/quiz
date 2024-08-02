f07f8070c90c52dddcb8ccd8446b4d08582ecc0f
Philip Henning <philiphenning@outlook.com>
2024-08-02 23:13:16 +0100

    logic used failed to deploy successfully, working on a fix - attempt 3

--- src/app/components/Question.jsx
+++ src/app/components/Question.jsx
@@ -12,26 +12,63 @@ export function Question({quizID, question_number, value, question, answer_1, an
     const router = useRouter()
     const { user } = useUser();
 
-    let [quit, setQuit] = useState(0)  
+    let [quit, setQuit] = useState(0)
+    let [answer, setAnswer] = useState(null)
     
     useEffect(() => {
 
         const userQuit = async () => {
-
             await AddUser(user.id, user.username)
             await UpdateLeaderboard(user.id, quizID, 2, score, (parseInt(question_number) -1) )
             router.push("/pages/static/quit")
         }
 
+        const win = async () => {
+            score = parseInt(score, 10) + value
+            await AddUser(user.id, user.username)
+            await UpdateLeaderboard(user.id, quizID, 1, score, question_number)
+            router.push("/pages/static/won")
+        }
+        
+        const correct = () => {
+            score = parseInt(score, 10) + value
+            router.push(`/pages/dynamic/quiz/${quizID}/${parseInt(question_number) +1}?score=${score}`)
+        }
+        
+        const lose = async () => {
+            await AddUser(user.id, user.username)
+            await UpdateLeaderboard(user.id, quizID, 3, score, (parseInt(question_number) -1))
+            router.push("/pages/static/lost")
+        }
+
+        // quit
         if(quit === 1){
            userQuit() 
         }
 
-    },[quit])
+        // win
+        else if(answer === final_answer && question_number === "15"){
+            win()
+        }
 
-        // Quit
+        // correct
+        else if(answer === final_answer){
+            correct()
+        }
+
+        // lose
+        else if(answer != null && answer != final_answer ){
+            lose()
+        }
+
+    },[quit, answer])
+
         function handleQuit(){
             setQuit(1)
+        }
+
+        function handleAnswer(selectedAnswer){
+            setAnswer(selectedAnswer)
         }
 
     return(
@@ -48,21 +85,21 @@ export function Question({quizID, question_number, value, question, answer_1, an
             <div className="AnswerBox">
                 <div className="AnswerTopRow">
                     <div className="QuestionLayout Answer">
-                        <button onClick={() => handleAnswer()}>{answer_1}</button>
+                        <button onClick={() => handleAnswer(answer_1)}>{answer_1}</button>
                     </div>
 
                     <div className="QuestionLayout Answer">
-                        <button onClick={() => handleAnswer()}>{answer_2}</button>
+                        <button onClick={() => handleAnswer(answer_2)}>{answer_2}</button>
                     </div>
                 </div>
 
                 <div className="AnswerBottomRow">
                     <div className="QuestionLayout Answer">
-                        <button onClick={() => handleAnswer()}>{answer_3}</button>
+                        <button onClick={() => handleAnswer(answer_3)}>{answer_3}</button>
                     </div>
 
                     <div className="QuestionLayout Answer">
-                        <button onClick={() => handleAnswer()}>{answer_4}</button>
+                        <button onClick={() => handleAnswer(answer_4)}>{answer_4}</button>
                     </div>
                 </div>
             </div>
