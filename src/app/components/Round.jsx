1e6e8c2ac8b6358ff622202268a49dae3e6bcd9c
Philip Henning <philiphenning@outlook.com>
2024-08-06 16:42:21 +0100

    fixed bug with scoreboard not displaying correctly

--- src/app/components/Round.jsx
+++ src/app/components/Round.jsx
@@ -3,26 +3,21 @@ export function Round({roundDetails, currentRound}){
     const round = JSON.parse(roundDetails)
 
     let value = 8.5
-    let win = null
-    let checkpoint = null
-    let current = null
-
-    console.log(roundDetails, currentRound)
 
     return(
         <>
             <div className="QuizProgress">
 
                 {round.map((item) => {
+                          
+                    let win = ""
+                    let checkpoint = ""
+                    let current = ""
 
                     value = value - .5
                
-                    {
-                        if(item.question_number === 15){win = "Win"}
-                        else if(item.question_number === 5 || item.question_number === 10){checkpoint = "Checkpoint"}
-                        else {win = "", checkpoint = "", current = ""}
-                    }
-                    
+                    if(item.question_number === 15){win = "Win"}
+                    if(item.question_number === 5 || item.question_number === 10){checkpoint = "Checkpoint"}
                     if(item.question_number == currentRound-1){current = "CurrentRound"}
 
                     return(
