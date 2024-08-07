3db5cc9e8363f5a3ce712d6a5d091740be8d4e7b
Philip Henning <philiphenning@outlook.com>
2024-08-07 11:30:38 +0100

    added 50:50 feature, but it doesn't persist over the game, only by round

--- src/app/components/Question.jsx
+++ src/app/components/Question.jsx
@@ -14,8 +14,13 @@ export function Question({quizID, question_number, value, question, answer_1, an
 
     let [quit, setQuit] = useState(0)
     let [answer, setAnswer] = useState(null)
+    let [askTheAudience,setAskTheAudience] = useState(0)
+    let [fiftyFifty, setFiftyFifty] = useState(0)
+    let [phoneAFriend, setPhoneAFriend] = useState(0)
     
     useEffect(() => {
+
+
 
         const userQuit = async () => {
             await AddUser(user.id, user.username)
@@ -43,25 +48,25 @@ export function Question({quizID, question_number, value, question, answer_1, an
 
         // quit
         if(quit === 1){
-           userQuit() 
-        }
+            userQuit() 
+            }
+    
+            // win
+            else if(answer === final_answer && question_number === "15"){
+                win()
+            }
+    
+            // correct
+            else if(answer === final_answer){
+                correct()
+            }
+    
+            // lose
+            else if(answer != null && answer != final_answer ){
+                lose()
+            }
 
-        // win
-        else if(answer === final_answer && question_number === "15"){
-            win()
-        }
-
-        // correct
-        else if(answer === final_answer){
-            correct()
-        }
-
-        // lose
-        else if(answer != null && answer != final_answer ){
-            lose()
-        }
-
-    },[quit, answer])
+    },[quit, answer, askTheAudience, fiftyFifty, phoneAFriend])
 
         function handleQuit(){
             setQuit(1)
@@ -71,55 +76,84 @@ export function Question({quizID, question_number, value, question, answer_1, an
             setAnswer(selectedAnswer)
         }
 
-        return(
-            <>  
+        function handleAskTheAudience(){
+            setAskTheAudience(1)
+            document.getElementById("askTheAudience").className = "Lifeline Unavailable"
+        }
 
-                <div className="QuizDetails">
+        function handleFiftyFifty(){
+            setFiftyFifty(1)
+            const fiftyFiftyArray = fiftyFiftyAnswers()
+            console.log(fiftyFiftyArray)
+            document.getElementById(`${fiftyFiftyArray[0]}`).className = "Hide"
+            document.getElementById(`${fiftyFiftyArray[1]}`).className = "Hide"
+            document.getElementById("fiftyFifty").className = "Lifeline Unavailable"
+        }
 
-                    <div className="QuestionLayout QuestionBar">
-                        <div className="Question">Question: {question}</div>
-                        <div className="QuestionNumber">{question_number} / 15</div>
+        function handlePhoneAFriend(){
+            setPhoneAFriend(1)
+            document.getElementById("phoneAFriend").className = "Lifeline Unavailable"
+        }
+
+        function fiftyFiftyAnswers(){
+            const answersArray = [answer_1, answer_2, answer_3, answer_4]
+            let temporaryArray = answersArray.filter(answer => answer !== final_answer)
+            temporaryArray = temporaryArray[Math.floor(Math.random()*temporaryArray.length)]
+            let toKeep = [temporaryArray, final_answer]
+            let fiftyfiftyAnswers = answersArray.filter(item => !toKeep.includes(item))
+            return fiftyfiftyAnswers
+        }
+
+    return(
+        <>  
+
+            <div className="QuizDetails">
+
+                <div className="QuestionLayout QuestionBar">
+                    <div className="Question">Question: {question}</div>
+                    <div className="QuestionNumber">{question_number} / 15</div>
+                </div>
+            
+                <div className="QuestionImage">
+                    <Image src={image} width={700} height={350} alt="question"  />
+                </div>
+
+                <div className="AnswerBox">
+                    <div className="AnswerTopRow">
+                        <div className="QuestionLayout Answer">
+                            <button id={answer_1} onClick={() => handleAnswer(answer_1)}>{answer_1}</button>
+                        </div>
+
+                        <div className="QuestionLayout Answer">
+                            <button id={answer_2} onClick={() => handleAnswer(answer_2)}>{answer_2}</button>
+                        </div>
                     </div>
+
+                    <div className="AnswerBottomRow">
+                        <div className="QuestionLayout Answer">
+                            <button id={answer_3} onClick={() => handleAnswer(answer_3)}>{answer_3}</button>
+                        </div>
+
+                        <div className="QuestionLayout Answer">
+                            <button id={answer_4} onClick={() => handleAnswer(answer_4)}>{answer_4}</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="QuizOptions">
+
+                <button id="askTheAudience" className="Lifeline Unavailable" onClick={ () => {handleAskTheAudience()}}>Ask the audience</button>
+
+                <button id="fiftyFifty" className="Lifeline Available" onClick={ () => {handleFiftyFifty()}}>50 : 50</button>
                 
-                    <div className="QuestionImage">
-                        <Image src={image} width={700} height={350} alt="question"  />
-                    </div>
+                <button id="phoneAFriend" className="Lifeline Unavailable" onClick={ () => {handlePhoneAFriend()}}>Phone a friend</button>
 
-                    <div className="AnswerBox">
-                        <div className="AnswerTopRow">
-                            <div className="QuestionLayout Answer">
-                                <button onClick={() => handleAnswer(answer_1)}>{answer_1}</button>
-                            </div>
+                <button className="Quit"onClick={() => handleQuit()}>QUIT</button>
 
-                            <div className="QuestionLayout Answer">
-                                <button onClick={() => handleAnswer(answer_2)}>{answer_2}</button>
-                            </div>
-                        </div>
+            </div>
 
-                        <div className="AnswerBottomRow">
-                            <div className="QuestionLayout Answer">
-                                <button onClick={() => handleAnswer(answer_3)}>{answer_3}</button>
-                            </div>
-
-                            <div className="QuestionLayout Answer">
-                                <button onClick={() => handleAnswer(answer_4)}>{answer_4}</button>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div className="QuizOptions">
-
-                    <button className="Lifeline">Ask the audience</button>
-
-                    <button className="Lifeline">50 : 50</button>
-                    
-                    <button className="Lifeline">Phone a friend</button>
-
-                    <button className="Quit"onClick={() => handleQuit()}>QUIT</button>
-                </div>
-
-            </>     
+        </>     
     )
 }
