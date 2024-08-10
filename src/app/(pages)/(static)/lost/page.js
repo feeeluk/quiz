c37b1f0fe8dbb637ef133b3f0249a22c7816d59e
Philip Henning <philiphenning@outlook.com>
2024-08-10 10:14:44 +0100

    updated the 'lose' page

--- src/app/(pages)/(static)/lost/page.js
+++ src/app/(pages)/(static)/lost/page.js
@@ -1,14 +1,32 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function Lose(){
     return(
         <>
             <h1>Wrong answer</h1>
+
+            <Image src={"https://media1.tenor.com/m/tVAgNJ6-mVAAAAAC/you-didn%27t-say-the-magic-word-jurassic-park.gif"} width={600} height={300} alt="image of Ned from Jurrasic Park"  />
+
             <p>
-                That was the wrong answer. Your progress will not be saved (although in a future version you will not lose everything, you will keep the score of your last checkpoint).</p>
-            <button>
-                <Link href="/home">OK</Link>
-            </button>              
+                That was the wrong answer. Your progress be saved.
+            </p>
+
+            <p>
+                In a future release you will not lose your entire score as you will keep the score based on your last checkpoint (rounds 5 & 10).
+            </p>
+
+            <p>
+                <button>
+                    <Link href="/home">Home</Link>
+                </button>
+
+                &nbsp;
+
+                <button>
+                    <Link href="/home">Leaderboard</Link>
+                </button>
+            </p>           
         </>
     )
 }
