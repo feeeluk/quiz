8e348b1df366dfa1c7a12a525515ce3818718571
Philip Henning <philiphenning@outlook.com>
2024-08-10 10:41:54 +0100

    updated the 'lose' page

--- src/app/(pages)/(static)/lost/page.js
+++ src/app/(pages)/(static)/lost/page.js
@@ -9,13 +9,15 @@ export default function Lose({searchParams}){
 
             <Image src={"https://media1.tenor.com/m/tVAgNJ6-mVAAAAAC/you-didn%27t-say-the-magic-word-jurassic-park.gif"} width={600} height={300} alt="image of Ned from Jurrasic Park"  />
 
-            <p>
-                That was the wrong answer. Your progress be saved even though you lost (quiz: {searchParams.quiz}, score: {searchParams.score}, round: {searchParams.round}).
-            </p>
+            <div className="Lose">
+                <p>
+                    That was the wrong answer. Your progress be saved even though you lost (quiz: {searchParams.quiz}, score: {searchParams.score}, round: {searchParams.round}).
+                </p>
 
-            <p>
-                In a future release you will not lose your entire score if you have made it past a checkpoint (rounds 5 & 10). Instead you will lose your current score and revert to the score of your last checkpoint.
-            </p>           
+                <p>
+                    In a future release you will not lose your entire score if you have made it past a checkpoint (rounds 5 & 10). Instead you will lose your current score and revert to the score of your last checkpoint.
+                </p>
+            </div>           
         </>
     )
 }
