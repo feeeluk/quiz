1be17c733829bcb8be6eb4f4c7ab20d49014feaf
Philip Henning <philiphenning@outlook.com>
2025-04-30 16:06:04 +0100

    changed footer wording

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,7 @@
 export function Footer(){
     return(
         <div className="Footer">
-            <p>This is V2 of WhoWantsToBeLessOfAPeasant? V1 was created as a showcase project - see About for more.</p>
+            <p>This is a rewrite of a showcase project - see 'About' for further details.</p>
         </div>
         
     )
