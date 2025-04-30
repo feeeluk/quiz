2816d81cf54433f5ba915e35c7de616a4b5164d2
Philip Henning <philiphenning@outlook.com>
2025-04-30 16:17:30 +0100

    changed the 'About' page wording - third attempt - using double backslash to escape special charters

--- src/app/(pages)/about/page.js
+++ src/app/(pages)/about/page.js
@@ -8,7 +8,7 @@ export default function About(){
             </div>
 
             <div>
-                <p>The orignial version of \'Who wants to be a peasant?\' was created in React as an end of term showcase project by Hannah Carter, William Stanforth-Sharpe and myself.</p>
+                <p>The orignial version of \\'Who wants to be a peasant?\\' was created in React as an end of term showcase project by Hannah Carter, William Stanforth-Sharpe and myself.</p>
 
                 <p>This (version 2) is my own project that I am building using Next.js, and gives me the opportunity to improve and expand upon what was a very short project.</p>
             </div>
